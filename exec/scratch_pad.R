ba343672f5fd6c47185cb012b56220b409312757
scoyoc <scoyoc@gmail.com>
2021-01-21 09:24:33 -0700

    Restructured raw data directory

--- exec/scratch_pad.R
+++ exec/scratch_pad.R
@@ -10,54 +10,17 @@ library("readr")
 library("stringi")
 library("janitor")
 
-#-- Testing import_file() --
-file.list <- list.files(path = "./inst/raw_data", pattern = ".csv",
-                        full.names = T, recursive = F)
-import_file(this_file = file.list[1], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-import_file(this_file = file.list[2], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-import_file(this_file = file.list[3], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-import_file(this_file = file.list[4], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-import_file(this_file = file.list[5], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-import_file(this_file = file.list[6], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-import_file(this_file = file.list[7], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-import_file(this_file = file.list[8], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-import_file(this_file = file.list[9], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-import_file(this_file = file.list[10], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-import_file(this_file = file.list[11], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-import_file(this_file = file.list[12], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-import_file(this_file = file.list[13], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-import_file(this_file = file.list[14], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-import_file(this_file = file.list[15], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-import_file(this_file = file.list[16], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-
-
-#-- Testing get_units()
-# PRCP, 4 cols
-my_file = import_file(this_file = file.list[1], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-# Temp, 4 cols
-my_file = import_file(this_file = file.list[2], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-# Temp, 5 cols
-my_file = import_file(this_file = file.list[4], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-my_file = import_file(this_file = file.list[16], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-my_file = import_file(this_file = file.list[15], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-my_file = import_file(this_file = file.list[9], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-my_file = import_file(this_file = file.list[12], datestamp_loc = 1, plotid_loc = 2, plotid_s = 1, plotid_e = 3)
-
-file.list <- list.files(path = "./inst/raw_data", pattern = ".csv",
-                        full.names = T, recursive = F)
-my_file <- import_file(file.list[1]); my_file
+# Testing weather data functions
+file_list <- list.files(path = system.file("extdata", package = "dataProcessor"),
+                        pattern = ".csv", full.names = TRUE, recursive = FALSE)
+my_file <- import_file(file_list[4]); my_file
 my_data <- get_data(my_file); my_data
 my_details <- get_details(my_file, my_data); my_details
-import_wxdat(file.list[1])
+import_wxdat(file_list[1])
 
-
-
-
-lapply(file.list, function(thisFile){
-  # Read file information and data
-  file_info = suppressWarnings(file_type(thisFile))
-  print(file_info)
-  suppressMessages(import_wxdat(thisFile))
+lapply(file_list, function(this_file){
+  x = import_wxdat(this_file)
+  print(x$file_info)
 })
 
 
