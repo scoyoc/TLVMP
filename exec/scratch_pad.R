2855e0b6536246416ec5dd68b6af183dc2a2527c
Mathew W. Van Scoyoc <mvanscoyoc@nps.gov>
2022-02-18 13:29:00 -0700

    Starting over

--- exec/scratch_pad.R
+++ exec/scratch_pad.R
@@ -9,3 +9,11 @@ devtools::load_all()
 # Testing weather data functions
 library("raindancer")
 
+my_dir <- "L:/LTVMP/Data/Weather Data/2008"
+file_list <- list.files(my_dir, pattern = ".csv", full.names = TRUE,
+                        recursive = FALSE)
+lapply(file_list, function(this_file){
+  x = raindancer::import_wxdat(this_file) |>
+    raindancer::process_hobo()
+  print(x)
+})
